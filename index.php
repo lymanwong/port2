b77634e105f1e350430c6dd950501a756ab3fbc8
lyman wong <lymanwong@gmail.com>
2017-06-04 13:19:57 -0700

    rem hover and click on alt images

--- index.php
+++ index.php
@@ -81,7 +81,7 @@
                 <div class="col-lg-4 col-md-4 text-center">
                     <div class="feature">
                         <!-- <i class="icon-lg ion-android-person wow fadeIn" data-wow-delay=".3s"></i> -->
-                        <a href="javascript:void(0)" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/neil.jpg"><img class="profile"src="images/neil.jpg" alt="This is a picture of Neil."></a>
+                        <a href="https://www.linkedin.com/in/lymanwong" class="gallery-box-alt" target="_blank data-toggle="modal" data-src="http://lymanwong.com/images/neil.jpg"><img class="profile"src="images/neil.jpg" alt="This is a picture of Neil."></a>
                         <h3>Neil F.</h3>
                         <h4>Sr. Director @ LeEco US</h4>
                         <p class="text-muted">"Launching LeEco in North America was no easy task and Lyman was a vital team member and leader in getting the business off the ground. LeEco's primary communication and sales channel is LeEco.com and Lyman was central to launching this. Lyman led development efforts to build and code a brand website..." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
@@ -90,7 +90,7 @@
                 <div class="col-lg-4 col-md-4 text-center">
                     <div class="feature">
                         <!-- <i class="icon-lg ion-android-person wow fadeInUp" data-wow-delay=".2s"></i> -->
-                        <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/Howard.jpg"><img class="profile" src="images/Howard.jpg" alt="This is a picture of my co-worker, Howard, at LeEco."></a>
+                        <a href="#galleryModal" class="gallery-box-alt" data-toggle="modal" data-src="http://lymanwong.com/images/Howard.jpg"><img class="profile" src="images/Howard.jpg" alt="This is a picture of my co-worker, Howard, at LeEco."></a>
                         <h3>Howard H.</h3>
                         <h4>Sr. Product Manager @ LeEco US</h4>
                         <p class="text-muted">"Persistent, thorough, and tenacious are three immediate words I would describe Lyman's professional grit. He had a project which involved launching a large scale eCommerce platform on a corporate site which he was able to handle and complete within the proposed timeline without skipping a beat..." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
@@ -99,7 +99,7 @@
                 <div class="col-lg-4 col-md-4 text-center">
                     <div class="feature">
                         <!-- <i class="icon-lg ion-android-person wow fadeIn" data-wow-delay=".3s"></i> -->
-                        <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/brandon.jpg"><img class="profile"src="images/brandon.jpg" alt="This is a picture of my friend, Eric, from Zynga."></a>
+                        <a href="#galleryModal" class="gallery-box-alt" data-toggle="modal" data-src="http://lymanwong.com/images/brandon.jpg"><img class="profile"src="images/brandon.jpg" alt="This is a picture of my friend, Eric, from Zynga."></a>
                         <h3>Brandon T.</h3>
                         <h4>Developer @ LeEco US</h4>
                         <p class="text-muted">"Anyone who knows Lyman will tell you that he's good person; reasonable and patient. He's got a clear passion for technology, teamwork and problem solving. Above all, he pushes himself hard and loves what he does. I enjoyed working with Lyman and wouldn't hesitate to choose him for my team – anytime." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
@@ -110,7 +110,7 @@
                 <div class="col-lg-4 col-md-4 text-center">
                     <div class="feature">
                         <!-- <i class="icon-lg ion-android-person wow fadeIn" data-wow-delay=".3s"></i> -->
-                        <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/alex.jpg"><img class="profile"src="images/alex.jpg" alt="This is a picture of my boss, Alex, speaking at a conference."></a>
+                        <a href="#galleryModal" class="gallery-box-alt" data-toggle="modal" data-src="http://lymanwong.com/images/alex.jpg"><img class="profile"src="images/alex.jpg" alt="This is a picture of my boss, Alex, speaking at a conference."></a>
                         <h3>Alex S.</h3>
                         <h4>Founder @ Phlint</h4>
                         <p class="text-muted">"Lyman is highly focused. He has experience in a variety of technical settings and keeps up to date on the latest technologies. His focused and disciplined attitude shows in his highly organized code base. Lyman is highly adaptable and a pleasure to work with." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
@@ -119,7 +119,7 @@
                 <div class="col-lg-4 col-md-4 text-center">
                     <div class="feature">
                         <!-- <i class="icon-lg ion-android-person wow fadeInUp" data-wow-delay=".2s"></i> -->
-                        <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/josh.jpg"><img class="profile" src="images/josh.jpg" alt="This is a picture of my friend, Josh, a co-worker at Zynga."></a>
+                        <a href="#galleryModal" class="gallery-box-alt" data-toggle="modal" data-src="http://lymanwong.com/images/josh.jpg"><img class="profile" src="images/josh.jpg" alt="This is a picture of my friend, Josh, a co-worker at Zynga."></a>
                         <h3>Josh W.</h3>
                         <h4>Backend Technical Lead @ Life360</h4>
                         <p class="text-muted">"While I can only select one title with which I've worked with Lyman, we have worked on the same projects for over three years and, across all of them he has, to use the parlance at Zynga, "killed it". He has always been completely reliable in his work, able to switch between projects and attentively follow them from start to finish..." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
@@ -128,7 +128,7 @@
                 <div class="col-lg-4 col-md-4 text-center">
                     <div class="feature">
                         <!-- <i class="icon-lg ion-android-person wow fadeIn" data-wow-delay=".3s"></i> -->
-                        <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/eric.jpg"><img class="profile"src="images/eric.jpg" alt="This is a picture of my friend, Eric, from Zynga."></a>
+                        <a href="#galleryModal" class="gallery-box-alt" data-toggle="modal" data-src="http://lymanwong.com/images/eric.jpg"><img class="profile"src="images/eric.jpg" alt="This is a picture of my friend, Eric, from Zynga."></a>
                         <h3>Eric A.</h3>
                         <h4>Systems Analyst @ Accenture</h4>
                         <p class="text-muted">"Truly, one of the best co-workers/teammates I've ever had. Lyman has a work-ethic that is pretty hard to match. He can carry a multitude of projects, simultaneously, from their beginning to end, pivot at a moments notice with rapidly changing needs, and without fail, maintain a forward-thinking, positive, friendly, approach to his work and to others..." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
