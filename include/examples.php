7f34563f69a59fac152e18f3b18a96de8f1dcdcd
lyman wong <lymanwong@gmail.com>
2017-06-06 15:13:25 -0700

    add text on card and rm ex card flip

--- include/examples.php
+++ include/examples.php
@@ -1,85 +1,93 @@
-    <section id="two" class="no-padding bg-primary">
-        <div class="container-fluid">
-            <div class="row no-gutter">
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/blog_lg.jpg" title="Click to see an example">
-                        <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a piture of a laptop.">
-                        <div class="gallery-box-caption">
-                            <div class="gallery-box-content">
-                                <div>
-                                    <h2>Phlint Retail Analytics</h2>
-                                    <h3>Checkout my Github for details</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/smarthomesf.jpg" title="Click to see an example">
-                        <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a picture of a laptop.">
-                        <div class="gallery-box-caption">
-                            <div class="gallery-box-content">
-                                <div>
-                                    <h2>Smart Home San Francisco</h2>
-                                    <h3>Checkout my Github for details</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/angular_example.jpg" title="Click to see an example">
-                        <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a picture of a laptop.">
-                        <div class="gallery-box-caption">
-                            <div class="gallery-box-content">
-                                <div>
-                                <h2>AngularJS</h2>
-                                <h4>https://lyman-wong-angular.herokuapp.com/#/</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/pinteresting.jpg" title="Click to see an example">
-                        <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a picture of a laptop.">
-                        <div class="gallery-box-caption">
-                            <div class="gallery-box-content">
-                                <div>
-                                    <h2>Rails</h2>
-                                    <h4>https://vrypinteresting.herokuapp.com/</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/picture_perfect.jpg" title="Click to see an example">
-                        <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a picture of a laptop.">
-                        <div class="gallery-box-caption">
-                            <div class="gallery-box-content">
-                                <div>
-                                    <h2>Freelance</h2>
-                                    <h4>https://pictureperfectbyivon.herokuapp.com/</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/paquito.png" title="Click to see an example">
-                        <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a picture of a laptop.">
-                        <div class="gallery-box-caption">
-                            <div class="gallery-box-content">
-                                <div>
-                                    <h2>Android</h2>
-                                    <h4>https://github.com/lymanwong/Android-Projects</h4>
-                                    <h3>Checkout my Github for more examples</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+<section id="two" class="no-padding bg-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="margin-top-0 text-primary">Examples</h2>
+                <hr class="primary">
             </div>
         </div>
-    </section>
+    </div>
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <div class="col-lg-4 col-sm-6">
+                <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/blog_lg.jpg" title="Click to see an example">
+                <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a piture of a laptop.">
+                <div class="gallery-box-caption">
+                    <div class="gallery-box-content">
+                        <div>
+                            <h2>Phlint Retail Analytics</h2>
+                            <h3>Checkout my Github for details</h3>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/smarthomesf.jpg" title="Click to see an example">
+                <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a picture of a laptop.">
+                <div class="gallery-box-caption">
+                    <div class="gallery-box-content">
+                        <div>
+                            <h2>Smart Home San Francisco</h2>
+                            <h3>Checkout my Github for details</h3>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/angular_example.jpg" title="Click to see an example">
+                <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a picture of a laptop.">
+                <div class="gallery-box-caption">
+                    <div class="gallery-box-content">
+                        <div>
+                            <h2>AngularJS</h2>
+                            <h4>https://lyman-wong-angular.herokuapp.com/#/</h4>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/pinteresting.jpg" title="Click to see an example">
+                <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a picture of a laptop.">
+                    <div class="gallery-box-caption">
+                        <div class="gallery-box-content">
+                            <div>
+                                <h2>Rails</h2>
+                                <h4>https://vrypinteresting.herokuapp.com/</h4>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/picture_perfect.jpg" title="Click to see an example">
+                <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a picture of a laptop.">
+                <div class="gallery-box-caption">
+                    <div class="gallery-box-content">
+                        <div>
+                            <h2>Freelance</h2>
+                            <h4>https://pictureperfectbyivon.herokuapp.com/</h4>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/paquito.png" title="Click to see an example">
+                <img src="http://lymanwong.com/images/laptop_med.jpg" class="img-responsive" alt="This is a picture of a laptop.">
+                <div class="gallery-box-caption">
+                    <div class="gallery-box-content">
+                        <div>
+                            <h2>Android</h2>
+                            <h4>https://github.com/lymanwong/Android-Projects</h4>
+                            <h3>Checkout my Github for more examples</h3>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
--- include/recommendations.php
+++ include/recommendations.php
@@ -17,6 +17,7 @@
                                 <a href="https://www.linkedin.com/in/lymanwong" class="gallery-box-alt" target="_blank data-toggle="modal" data-src="http://lymanwong.com/images/neil.jpg"><img class="profile"src="images/neil.jpg" alt="This is a picture of Neil."></a>
                                 <h3>Neil F.</h3>
                                 <h4>Sr. Director @ LeEco US</h4>
+                                <h4><a href="https://www.linkedin.com/in/lymanwong" target="_blank">(See what Neil had to say)</a></h4>
                             </div>
                             <div class="back">
                                 <p class="text-muted">"Launching LeEco in North America was no easy task and Lyman was a vital team member and leader in getting the business off the ground. LeEco's primary communication and sales channel is LeEco.com and Lyman was central to launching this. Lyman led development efforts to build and code a brand website..." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
@@ -32,6 +33,7 @@
                         <a href="#galleryModal" class="gallery-box-alt" data-toggle="modal" data-src="http://lymanwong.com/images/chen.jpg"><img class="profile" src="images/chen.jpg" alt="This is a picture of my  co-worker at LeEco, Chen."></a>
                         <h3>Chen F.</h3>
                         <h4>Software Engineer @ LeEco</h4>
+                        <h4><a href="https://www.linkedin.com/in/lymanwong" target="_blank">(See what Chen had to say)</a></h4>
                     </div>
                     <div class="back">
                        <p class="text-muted">"It's a great pleasure to work with Lyman. He's one of the smartest, intelligent, and modest man that who could do the job right and completely. He's responsibleness and capabilities build the trust in the team, and he's kindness and humor make everyone like to work with him. Hope everything goes well at he's new position, and it's always being fun to work him." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
@@ -47,6 +49,7 @@
                         <a href="#galleryModal" class="gallery-box-alt" data-toggle="modal" data-src="http://lymanwong.com/images/brandon.jpg"><img class="profile"src="images/brandon.jpg" alt="This is a picture of my friend, Eric, from Zynga."></a>
                         <h3>Brandon T.</h3>
                         <h4>Software Engineer @ LeEco US</h4>
+                        <h4><a href="https://www.linkedin.com/in/lymanwong" target="_blank">(See what Brandon had to say)</a></h4>
                     </div>
                     <div class="back">
                         <p class="text-muted">"Anyone who knows Lyman will tell you that he's good person; reasonable and patient. He's got a clear passion for technology, teamwork and problem solving. Above all, he pushes himself hard and loves what he does. I enjoyed working with Lyman and wouldn't hesitate to choose him for my team – anytime." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
@@ -64,6 +67,7 @@
                         <a href="#galleryModal" class="gallery-box-alt" data-toggle="modal" data-src="http://lymanwong.com/images/alex.jpg"><img class="profile"src="images/alex.jpg" alt="This is a picture of my boss, Alex, speaking at a conference."></a>
                         <h3>Alex S.</h3>
                         <h4>Founder @ Phlint</h4>
+                        <h4><a href="https://www.linkedin.com/in/lymanwong" target="_blank">(See what Alex had to say)</a></h4>
                     </div>
                     <div class="back">
                         <p class="text-muted">"Lyman is highly focused. He has experience in a variety of technical settings and keeps up to date on the latest technologies. His focused and disciplined attitude shows in his highly organized code base. Lyman is highly adaptable and a pleasure to work with." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
@@ -79,6 +83,7 @@
                         <a href="#galleryModal" class="gallery-box-alt" data-toggle="modal" data-src="http://lymanwong.com/images/Howard.jpg"><img class="profile" src="images/Howard.jpg" alt="This is a picture of my co-worker, Howard, at LeEco."></a>
                         <h3>Howard H.</h3>
                         <h4>Sr. Product Manager @ LeEco US</h4>
+                        <h4><a href="https://www.linkedin.com/in/lymanwong" target="_blank">(See what Howard had to say)</a></h4>
                     </div>
                     <div class="back">
                                <p class="text-muted">"Persistent, thorough, and tenacious are three immediate words I would describe Lyman's professional grit. He had a project which involved launching a large scale eCommerce platform on a corporate site which he was able to handle and complete within the proposed timeline without skipping a beat..." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
@@ -94,6 +99,7 @@
                         <a href="#galleryModal" class="gallery-box-alt" data-toggle="modal" data-src="http://lymanwong.com/images/shan.jpg"><img class="profile"src="images/shan.jpg" alt="This is a picture of my coworker from LeEco, Shan."></a>
                         <h3>Shan G.</h3>
                         <h4>User Experience Designer @ LeEco</h4>
+                        <h4><a href="https://www.linkedin.com/in/lymanwong" target="_blank">(See what Shan had to say)</a></h4>
                     </div>
                     <div class="back">
                         <p class="text-muted">"Lyman does not only have comprehensive technical skill sets, but also sees where programming and user experience meet. He has the ability to interpret the UX/UI designs and implementing them effectively. He always checked with designers on any details of the designs in order to launching a successful and functioning website. Also, Lyman is a truly team player and a great communicator,..." <a href="https://www.linkedin.com/in/lymanwong" target="_blank">(more)</a></p>
