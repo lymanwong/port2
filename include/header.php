3fbb71a17b01ad6fa489739b28c4f0b80c7e4753
lyman wong <lymanwong@gmail.com>
2017-09-19 14:29:07 -0700

    add media queries

--- include/header.php
+++ include/header.php
@@ -7,7 +7,4 @@
         </div>
     </div>
   </div>
-<!--     <video autoplay loop muted class="fillWidth fadeIn wow collapse in" data-wow-delay="0.5s" poster="http://lymanwong.com/images/laptop.jpg" id="video-background">
-        <source src="http://lymanwong.com/videos/Laptop_1_Videvo.mp4" type="video/mp4">Your browser does not support the video tag. I suggest you upgrade your browser.
-    </video> -->
 </header>
