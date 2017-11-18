3c55430af0f6464fae64debc29375a89660d4a42
lyman wong <lymanwong@gmail.com>
2017-11-18 15:58:13 -0800

    fixed css for header

--- include/header.php
+++ include/header.php
@@ -3,7 +3,7 @@
     <div class="header-content">
         <div class="inner">
             <a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="http://lymanwong.com/images/profile.jpg"><img class="profile" src="http://lymanwong.com/images/profile.jpg" alt="This is a headshot of me."></a>
-            <h1 class="cursive stroke_me">Lyman Wong<br>Web Developer</h1>
+            <h1 class="cursive stroke_me">Lyman Wong<br>Web Developer & Community Guru</h1>
         </div>
     </div>
   </div>
